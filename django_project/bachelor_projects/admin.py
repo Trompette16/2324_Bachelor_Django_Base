8c3f15be2f7b1a16488844f70e01c34ab161de7d
SwifferSK <theas.damien@gmail.com>
2024-05-21 14:58:29 +0200

    update Database

--- django_project/bachelor_projects/admin.py
+++ django_project/bachelor_projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bachelor_projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
+
 
 # Register your models here.
